5bc6702c0d7ee0a6935fabb841b5c2de0b415601
Kadir Can Ozden <101993364+bysiber@users.noreply.github.com>
2026-07-13 02:49:44 +0300

    Update cask to v1.8.0

--- Casks/cleardisk.rb
+++ Casks/cleardisk.rb
@@ -1,13 +1,13 @@
 cask "cleardisk" do
-  version "1.7.1"
-  sha256 "5f32e2b928019e1ed6e0671ddff311a3688e35bd89282f94439a4d8eeb5161ea"
+  version "1.8.0"
+  sha256 "30b6e7a8108da746ef6215eea5c69286679daaf342ee2fe5fd0bc1c066ae9599"
 
   url "https://github.com/bysiber/cleardisk/releases/download/v#{version}/ClearDisk-v#{version}.dmg"
   name "ClearDisk"
   desc "Free, open-source macOS app to find and clean developer caches"
   homepage "https://github.com/bysiber/cleardisk"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "ClearDisk.app"
 
@@ -18,7 +18,8 @@ cask "cleardisk" do
   end
 
   zap trash: [
-    "~/Library/Preferences/com.bysiber.ClearDisk.plist",
-    "~/Library/Caches/com.bysiber.ClearDisk",
+    "~/Library/Preferences/com.cleardisk.app.plist",
+    "~/Library/Caches/com.cleardisk.app",
+    "~/Library/Saved Application State/com.cleardisk.app.savedState",
   ]
 end
