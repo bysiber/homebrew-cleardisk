67aaf6bc21a8a1fedfdc02f557ae71e1bfc5a538
Kadir Can Ozden <101993364+bysiber@users.noreply.github.com>
2026-08-26 13:34:21 +0000

    Update cask to v2.0.0

--- Casks/cleardisk.rb
+++ Casks/cleardisk.rb
@@ -1,12 +1,13 @@
 cask "cleardisk" do
-  version "1.9.0"
-  sha256 "eb5806ef5008074c7d1d186e2d4337ba41b5a9b567f4e67a14d1bcb118446243"
+  version "2.0.0"
+  sha256 "fac2c83692f8bbdf92f3f039034416a87e1bab132cd1f1c2817bc3a7cd6dbbdf"
 
   url "https://github.com/bysiber/cleardisk/releases/download/v#{version}/ClearDisk-v#{version}.dmg"
   name "ClearDisk"
   desc "Free, open-source macOS app to find and clean developer caches"
   homepage "https://github.com/bysiber/cleardisk"
 
+  auto_updates true
   depends_on macos: :sonoma
 
   app "ClearDisk.app"
